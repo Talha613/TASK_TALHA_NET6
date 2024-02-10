39f2155807b3455aa75f99d15f3dfdfdffa5b03d
Administrator <Administrator@DESKTOP-FUGQNF4>
2024-02-10 18:03:47 +0500

    Add project files.

---
+++ Task_Talha/Controllers/DataController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_Talha.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var items = new List<Item>
+        {
+            new Item { Id = 1, Name = "Item 1" },
+            new Item { Id = 2, Name = "Item 2" },
+            new Item { Id = 3, Name = "Item 3" },
+            new Item { Id = 4, Name = "Item 4" },
+            new Item { Id = 5, Name = "Item 5" },
+            new Item { Id = 6, Name = "Item 6" },
+
+        };
+
+            return Ok(items);
+        }
+
+        public class Item
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
